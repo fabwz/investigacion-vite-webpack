a24ad3e2bcf255f3f263875c84207d1aa9c7d10d
fabwz <fabianzaar@gmail.com>
2026-07-27 23:37:04 -0600

    feat(main): implementar main.js con logica de HMR para Vite

---
+++ src/main.js
@@ -0,0 +1,402 @@
+// ============================================================
+// main.js - Punto de entrada de la aplicacion
+// ============================================================
+// Este archivo importa los 120 componentes generados y los monta
+// dentro de #app. Se usan imports explicitos (uno por componente)
+// en lugar de import.meta.glob para que el flujo sea facil de
+// explicar en vivo: cada linea es un import de ES modules estandar,
+// sin magia especifica de un bundler.
+
+import { createItem001 } from './components/generated/item001.js';
+import { createItem002 } from './components/generated/item002.js';
+import { createItem003 } from './components/generated/item003.js';
+import { createItem004 } from './components/generated/item004.js';
+import { createItem005 } from './components/generated/item005.js';
+import { createItem006 } from './components/generated/item006.js';
+import { createItem007 } from './components/generated/item007.js';
+import { createItem008 } from './components/generated/item008.js';
+import { createItem009 } from './components/generated/item009.js';
+import { createItem010 } from './components/generated/item010.js';
+import { createItem011 } from './components/generated/item011.js';
+import { createItem012 } from './components/generated/item012.js';
+import { createItem013 } from './components/generated/item013.js';
+import { createItem014 } from './components/generated/item014.js';
+import { createItem015 } from './components/generated/item015.js';
+import { createItem016 } from './components/generated/item016.js';
+import { createItem017 } from './components/generated/item017.js';
+import { createItem018 } from './components/generated/item018.js';
+import { createItem019 } from './components/generated/item019.js';
+import { createItem020 } from './components/generated/item020.js';
+import { createItem021 } from './components/generated/item021.js';
+import { createItem022 } from './components/generated/item022.js';
+import { createItem023 } from './components/generated/item023.js';
+import { createItem024 } from './components/generated/item024.js';
+import { createItem025 } from './components/generated/item025.js';
+import { createItem026 } from './components/generated/item026.js';
+import { createItem027 } from './components/generated/item027.js';
+import { createItem028 } from './components/generated/item028.js';
+import { createItem029 } from './components/generated/item029.js';
+import { createItem030 } from './components/generated/item030.js';
+import { createItem031 } from './components/generated/item031.js';
+import { createItem032 } from './components/generated/item032.js';
+import { createItem033 } from './components/generated/item033.js';
+import { createItem034 } from './components/generated/item034.js';
+import { createItem035 } from './components/generated/item035.js';
+import { createItem036 } from './components/generated/item036.js';
+import { createItem037 } from './components/generated/item037.js';
+import { createItem038 } from './components/generated/item038.js';
+import { createItem039 } from './components/generated/item039.js';
+import { createItem040 } from './components/generated/item040.js';
+import { createItem041 } from './components/generated/item041.js';
+import { createItem042 } from './components/generated/item042.js';
+import { createItem043 } from './components/generated/item043.js';
+import { createItem044 } from './components/generated/item044.js';
+import { createItem045 } from './components/generated/item045.js';
+import { createItem046 } from './components/generated/item046.js';
+import { createItem047 } from './components/generated/item047.js';
+import { createItem048 } from './components/generated/item048.js';
+import { createItem049 } from './components/generated/item049.js';
+import { createItem050 } from './components/generated/item050.js';
+import { createItem051 } from './components/generated/item051.js';
+import { createItem052 } from './components/generated/item052.js';
+import { createItem053 } from './components/generated/item053.js';
+import { createItem054 } from './components/generated/item054.js';
+import { createItem055 } from './components/generated/item055.js';
+import { createItem056 } from './components/generated/item056.js';
+import { createItem057 } from './components/generated/item057.js';
+import { createItem058 } from './components/generated/item058.js';
+import { createItem059 } from './components/generated/item059.js';
+import { createItem060 } from './components/generated/item060.js';
+import { createItem061 } from './components/generated/item061.js';
+import { createItem062 } from './components/generated/item062.js';
+import { createItem063 } from './components/generated/item063.js';
+import { createItem064 } from './components/generated/item064.js';
+import { createItem065 } from './components/generated/item065.js';
+import { createItem066 } from './components/generated/item066.js';
+import { createItem067 } from './components/generated/item067.js';
+import { createItem068 } from './components/generated/item068.js';
+import { createItem069 } from './components/generated/item069.js';
+import { createItem070 } from './components/generated/item070.js';
+import { createItem071 } from './components/generated/item071.js';
+import { createItem072 } from './components/generated/item072.js';
+import { createItem073 } from './components/generated/item073.js';
+import { createItem074 } from './components/generated/item074.js';
+import { createItem075 } from './components/generated/item075.js';
+import { createItem076 } from './components/generated/item076.js';
+import { createItem077 } from './components/generated/item077.js';
+import { createItem078 } from './components/generated/item078.js';
+import { createItem079 } from './components/generated/item079.js';
+import { createItem080 } from './components/generated/item080.js';
+import { createItem081 } from './components/generated/item081.js';
+import { createItem082 } from './components/generated/item082.js';
+import { createItem083 } from './components/generated/item083.js';
+import { createItem084 } from './components/generated/item084.js';
+import { createItem085 } from './components/generated/item085.js';
+import { createItem086 } from './components/generated/item086.js';
+import { createItem087 } from './components/generated/item087.js';
+import { createItem088 } from './components/generated/item088.js';
+import { createItem089 } from './components/generated/item089.js';
+import { createItem090 } from './components/generated/item090.js';
+import { createItem091 } from './components/generated/item091.js';
+import { createItem092 } from './components/generated/item092.js';
+import { createItem093 } from './components/generated/item093.js';
+import { createItem094 } from './components/generated/item094.js';
+import { createItem095 } from './components/generated/item095.js';
+import { createItem096 } from './components/generated/item096.js';
+import { createItem097 } from './components/generated/item097.js';
+import { createItem098 } from './components/generated/item098.js';
+import { createItem099 } from './components/generated/item099.js';
+import { createItem100 } from './components/generated/item100.js';
+import { createItem101 } from './components/generated/item101.js';
+import { createItem102 } from './components/generated/item102.js';
+import { createItem103 } from './components/generated/item103.js';
+import { createItem104 } from './components/generated/item104.js';
+import { createItem105 } from './components/generated/item105.js';
+import { createItem106 } from './components/generated/item106.js';
+import { createItem107 } from './components/generated/item107.js';
+import { createItem108 } from './components/generated/item108.js';
+import { createItem109 } from './components/generated/item109.js';
+import { createItem110 } from './components/generated/item110.js';
+import { createItem111 } from './components/generated/item111.js';
+import { createItem112 } from './components/generated/item112.js';
+import { createItem113 } from './components/generated/item113.js';
+import { createItem114 } from './components/generated/item114.js';
+import { createItem115 } from './components/generated/item115.js';
+import { createItem116 } from './components/generated/item116.js';
+import { createItem117 } from './components/generated/item117.js';
+import { createItem118 } from './components/generated/item118.js';
+import { createItem119 } from './components/generated/item119.js';
+import { createItem120 } from './components/generated/item120.js';
+
+const componentFactories = [
+  createItem001,
+  createItem002,
+  createItem003,
+  createItem004,
+  createItem005,
+  createItem006,
+  createItem007,
+  createItem008,
+  createItem009,
+  createItem010,
+  createItem011,
+  createItem012,
+  createItem013,
+  createItem014,
+  createItem015,
+  createItem016,
+  createItem017,
+  createItem018,
+  createItem019,
+  createItem020,
+  createItem021,
+  createItem022,
+  createItem023,
+  createItem024,
+  createItem025,
+  createItem026,
+  createItem027,
+  createItem028,
+  createItem029,
+  createItem030,
+  createItem031,
+  createItem032,
+  createItem033,
+  createItem034,
+  createItem035,
+  createItem036,
+  createItem037,
+  createItem038,
+  createItem039,
+  createItem040,
+  createItem041,
+  createItem042,
+  createItem043,
+  createItem044,
+  createItem045,
+  createItem046,
+  createItem047,
+  createItem048,
+  createItem049,
+  createItem050,
+  createItem051,
+  createItem052,
+  createItem053,
+  createItem054,
+  createItem055,
+  createItem056,
+  createItem057,
+  createItem058,
+  createItem059,
+  createItem060,
+  createItem061,
+  createItem062,
+  createItem063,
+  createItem064,
+  createItem065,
+  createItem066,
+  createItem067,
+  createItem068,
+  createItem069,
+  createItem070,
+  createItem071,
+  createItem072,
+  createItem073,
+  createItem074,
+  createItem075,
+  createItem076,
+  createItem077,
+  createItem078,
+  createItem079,
+  createItem080,
+  createItem081,
+  createItem082,
+  createItem083,
+  createItem084,
+  createItem085,
+  createItem086,
+  createItem087,
+  createItem088,
+  createItem089,
+  createItem090,
+  createItem091,
+  createItem092,
+  createItem093,
+  createItem094,
+  createItem095,
+  createItem096,
+  createItem097,
+  createItem098,
+  createItem099,
+  createItem100,
+  createItem101,
+  createItem102,
+  createItem103,
+  createItem104,
+  createItem105,
+  createItem106,
+  createItem107,
+  createItem108,
+  createItem109,
+  createItem110,
+  createItem111,
+  createItem112,
+  createItem113,
+  createItem114,
+  createItem115,
+  createItem116,
+  createItem117,
+  createItem118,
+  createItem119,
+  createItem120,
+];
+
+const appRoot = document.getElementById('app');
+
+function renderApp() {
+  appRoot.innerHTML = '';
+
+  const fragment = document.createDocumentFragment();
+  for (const createItem of componentFactories) {
+    fragment.appendChild(createItem());
+  }
+  appRoot.appendChild(fragment);
+}
+
+renderApp();
+
+// Hot Module Replacement (HMR) - rama especifica de Vite
+
+if (import.meta.hot) {
+
+  const componentPaths = [
+    './components/generated/item001.js',
+    './components/generated/item002.js',
+    './components/generated/item003.js',
+    './components/generated/item004.js',
+    './components/generated/item005.js',
+    './components/generated/item006.js',
+    './components/generated/item007.js',
+    './components/generated/item008.js',
+    './components/generated/item009.js',
+    './components/generated/item010.js',
+    './components/generated/item011.js',
+    './components/generated/item012.js',
+    './components/generated/item013.js',
+    './components/generated/item014.js',
+    './components/generated/item015.js',
+    './components/generated/item016.js',
+    './components/generated/item017.js',
+    './components/generated/item018.js',
+    './components/generated/item019.js',
+    './components/generated/item020.js',
+    './components/generated/item021.js',
+    './components/generated/item022.js',
+    './components/generated/item023.js',
+    './components/generated/item024.js',
+    './components/generated/item025.js',
+    './components/generated/item026.js',
+    './components/generated/item027.js',
+    './components/generated/item028.js',
+    './components/generated/item029.js',
+    './components/generated/item030.js',
+    './components/generated/item031.js',
+    './components/generated/item032.js',
+    './components/generated/item033.js',
+    './components/generated/item034.js',
+    './components/generated/item035.js',
+    './components/generated/item036.js',
+    './components/generated/item037.js',
+    './components/generated/item038.js',
+    './components/generated/item039.js',
+    './components/generated/item040.js',
+    './components/generated/item041.js',
+    './components/generated/item042.js',
+    './components/generated/item043.js',
+    './components/generated/item044.js',
+    './components/generated/item045.js',
+    './components/generated/item046.js',
+    './components/generated/item047.js',
+    './components/generated/item048.js',
+    './components/generated/item049.js',
+    './components/generated/item050.js',
+    './components/generated/item051.js',
+    './components/generated/item052.js',
+    './components/generated/item053.js',
+    './components/generated/item054.js',
+    './components/generated/item055.js',
+    './components/generated/item056.js',
+    './components/generated/item057.js',
+    './components/generated/item058.js',
+    './components/generated/item059.js',
+    './components/generated/item060.js',
+    './components/generated/item061.js',
+    './components/generated/item062.js',
+    './components/generated/item063.js',
+    './components/generated/item064.js',
+    './components/generated/item065.js',
+    './components/generated/item066.js',
+    './components/generated/item067.js',
+    './components/generated/item068.js',
+    './components/generated/item069.js',
+    './components/generated/item070.js',
+    './components/generated/item071.js',
+    './components/generated/item072.js',
+    './components/generated/item073.js',
+    './components/generated/item074.js',
+    './components/generated/item075.js',
+    './components/generated/item076.js',
+    './components/generated/item077.js',
+    './components/generated/item078.js',
+    './components/generated/item079.js',
+    './components/generated/item080.js',
+    './components/generated/item081.js',
+    './components/generated/item082.js',
+    './components/generated/item083.js',
+    './components/generated/item084.js',
+    './components/generated/item085.js',
+    './components/generated/item086.js',
+    './components/generated/item087.js',
+    './components/generated/item088.js',
+    './components/generated/item089.js',
+    './components/generated/item090.js',
+    './components/generated/item091.js',
+    './components/generated/item092.js',
+    './components/generated/item093.js',
+    './components/generated/item094.js',
+    './components/generated/item095.js',
+    './components/generated/item096.js',
+    './components/generated/item097.js',
+    './components/generated/item098.js',
+    './components/generated/item099.js',
+    './components/generated/item100.js',
+    './components/generated/item101.js',
+    './components/generated/item102.js',
+    './components/generated/item103.js',
+    './components/generated/item104.js',
+    './components/generated/item105.js',
+    './components/generated/item106.js',
+    './components/generated/item107.js',
+    './components/generated/item108.js',
+    './components/generated/item109.js',
+    './components/generated/item110.js',
+    './components/generated/item111.js',
+    './components/generated/item112.js',
+    './components/generated/item113.js',
+    './components/generated/item114.js',
+    './components/generated/item115.js',
+    './components/generated/item116.js',
+    './components/generated/item117.js',
+    './components/generated/item118.js',
+    './components/generated/item119.js',
+    './components/generated/item120.js',
+  ];
+
+  import.meta.hot.accept(componentPaths, () => {
+
+    renderApp();
+  });
+}
+
+
+
